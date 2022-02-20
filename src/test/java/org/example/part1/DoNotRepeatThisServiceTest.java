b3e9eb8d15cd91ba0341be37012f2513d7554c1e
kotbajan <kotbajan@localhist>
2022-02-20 19:35:53 +0300

    part1: CountDownLatch test

--- src/test/java/org/example/part1/DoNotRepeatThisServiceTest.java
+++ src/test/java/org/example/part1/DoNotRepeatThisServiceTest.java
@@ -3,18 +3,24 @@ package org.example.part1;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 
 @Slf4j
 @SpringJUnitConfig
@@ -31,6 +37,14 @@ class DoNotRepeatThisServiceTest {
     @Autowired
     DoNotRepeatThisService service;
 
+    @SpyBean
+    Transformer transformer;
+
+    @BeforeEach
+    void setUp() {
+        Mockito.reset(transformer);
+    }
+
     @Test
     public void functionalTest() {
         String key = "1";
@@ -44,7 +58,7 @@ class DoNotRepeatThisServiceTest {
     public void stressTest() {
         Instant start = Instant.now();
 
-        IntStream.range(1, 1000)
+        IntStream.rangeClosed(1, 1000)
                 .boxed()
                 .map(Object::toString)
                 .parallel()
@@ -64,7 +78,35 @@ class DoNotRepeatThisServiceTest {
     public void stressWithCheckTest() {
         SoftAssertions softAssertions = new SoftAssertions();
 
-        IntStream.range(1, 1000)
+        IntStream.rangeClosed(1, 1000)
+                .boxed()
+                .map(Object::toString)
+                .parallel()
+                .forEach(it -> {
+                    Integer result = service.execute(it);
+                    Integer expected = calcExpected(it);
+                    softAssertions.assertThat(result)
+                            .isEqualTo(expected);
+                });
+
+        softAssertions.assertAll();
+    }
+
+    @Test
+    public void countDownTest() {
+        CountDownLatch cdl = new CountDownLatch(2);
+        Mockito.doAnswer(invocation -> {
+            cdl.countDown();
+            final boolean await = cdl.await(5, TimeUnit.SECONDS);
+            if (!await) {
+                throw new RuntimeException("Не дождались 2го потока");
+            }
+            return invocation.callRealMethod();
+        }).when(transformer).transform(any());
+
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        IntStream.rangeClosed(1, 2)
                 .boxed()
                 .map(Object::toString)
                 .parallel()
